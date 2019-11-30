ed098cd90a5dd6f987c775a0f92155e54cf8afd2
Atique Ahmed ANMEPL BLR <atique.ahmed@asianetnews.in>
2019-11-30 12:30:43 +0530

    backedn

--- form/backend/controllers/personController.js
+++ form/backend/controllers/personController.js
@@ -12,15 +12,15 @@ router.get('/',(req,res) =>{
     })
 })
 
-router.get('/:id',(req,res) => {
-     if(!ObjectId.isValid(req.params.id))
-     return res.status(400).send('No record with given id: $(req.params.id)');
+// router.get('/:id',(req,res) => {
+//      if(!ObjectId.isValid(req.params.id))
+//      return res.status(400).send('No record with given id: $(req.params.id)');
 
-     Person.findById(req.params.id, (err, docs) => {
-         if(!err){res.send(docs)}
-         else{ console.log("Erroe in retrieving people:" + JSON.stringify(err, undefined, 2)) }
-        });
-})
+//      Person.findById(req.params.id, (err, docs) => {
+//          if(!err){res.send(docs)}
+//          else{ console.log("Erroe in retrieving people:" + JSON.stringify(err, undefined, 2)) }
+//         });
+// })
 
 router.post('/',(req,res) => {
     var per = new Person({
@@ -28,23 +28,55 @@ router.post('/',(req,res) => {
         mail : req.body.mail,
         class : req.body.class
     });
-    per.save((err, doc) => {
-        if(!err){res.send(doc)}
-        else { console.log('Error in retrieving persons' + JSON.stringify(err, undefined, 2)); }
-    });
+    console.log(req.body.mail);
+    // Demo
+    Person.findOne({'mail':req.body.mail}, (err, docs) => {
+        if(!docs) {
+            per.save((err, doc) => {
+                if(!err){
+                    res.status(200).send({ auth: true, doc, message:"1 documents inserted!" });
+                }
+                else { console.log('Error in user inserting data' + JSON.stringify(err, undefined, 2)); }
+            });
+        }
+        else { 
+            console.log("User data already exists:"+req.body.mail);
+            res.status(400).send({
+                message: 'User data already exists:'+req.body.mail
+            }) 
+        }
+    })
+    // Demo
+    // per.save((err, doc) => {
+    //     if(!err){res.send(doc)}
+    //     else { console.log('Error in retrieving persons' + JSON.stringify(err, undefined, 2)); }
+    // });
 })
 router.put('/:id',(req,res) => {
     if(!ObjectId.isValid(req.params.id))
      return res.status(400).send('No record with given id: $(req.params.id)');
 
-     var per = new Person({
-        name : req.body.name,
-        mail : req.body.mail,
-        class : req.body.class
-    });
-    Person.findByIdAndUpdate(req.params.id,{ $set : per },{ new : true }, (err,docs) => {
-        if(!err){ res.send(docs); }
-        else { console.log("Error in person updation" + JSON.stringify(err, undefined, 2)); }
+     var per = {
+        $set: {
+            name : req.body.name,
+            mail : req.body.mail,
+            class : req.body.class
+        }
+    };
+    Person.findOneAndUpdate({mail: req.body.mail}, per, {new: true, useFindAndModify: false}, (err, doc) => {
+        if (err) {
+            console.log("Something wrong when updating data!");
+        } else if(doc) {
+            res.status(200).send({
+                auth: true,
+                message:"1 document updated"
+            })
+        } else {
+            res.status(404).send({
+                message:"Resource not found, please register first with the email id!!"
+            })
+        }
+        console.log(doc);
     });
 })
 
@@ -52,7 +84,7 @@ router.delete('/:id',(req,res) => {
     if(!ObjectId.isValid(req.params.id))
      return res.status(400).send('No record with given id: $(req.params.id)');
      
-    Person.findByIdAndRemove(req.params.id, (err,docs) => {
+    Person.findByIdAndRemove(req.params.id, {new: true, useFindAndModify: false}, (err,docs) => {
         if(!err){ res.send(docs); }
         else { console.log("Error in person updation" + JSON.stringify(err, undefined, 2)); }
     });
--- form/backend/db.js
+++ form/backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost:27017/firstdb',(err) => {
+mongoose.connect('mongodb://localhost:27017/firstdb', { useNewUrlParser:true, useUnifiedTopology: true }, (err) => {
     if(!err){
         console.log('MongoDB connected...')
     } else
