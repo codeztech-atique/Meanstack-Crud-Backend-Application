a4e23adc39ebae6cf7047c3bdb7a08b1638c0a59
Atique Ahmed <atique1201@gmail.com>
2020-01-03 15:00:44 +0000

    index.js edited online with Bitbucket

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const personController = require('./controllers/personController.js')
  var app = express();
  app.use(bodyParser.json());
  app.use(cors({ origin : 'http://localhost:4200' }));
+ 
+ //Application testing API's
+ app.get('/app/testing',(req,res) =>{
+    res.send({
+      "message":"App is working fine",
+      "status":200
+    })
+ })
 
  app.listen(3000, () => console.log('Server connected to port: 3000'))
 
